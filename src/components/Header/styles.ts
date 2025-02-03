2ae3efed740a0dceb09d0889dc74346c233eac64
Gustavo <gustavoburgerf@gmail.com>
2025-02-03 11:53:30 -0300

    adicionando o banner

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -4,9 +4,11 @@ import { cores } from '../../styles'
 export const HeaderBar = styled.header`
   background-color: ${cores.cinza};
   padding: 24px;
+  margin-top: 40px;
   margin-bottom: 80px;
   display: flex;
   align-items: center;
+  border-radius: 16px;
   justify-content: space-between;
 
   a {
