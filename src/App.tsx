64963662b86782956eb7690f2bcb61037b7cb39e
Gustavo <gustavoburgerf@gmail.com>
2025-02-19 21:23:59 -0300

    criando o carrinho

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { GlobalCss } from './styles'
 import Footer from './components/Footer'
 import Rotas from './routes'
 import { store } from './store'
+import Cart from './components/Cart'
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         </div>
         <Rotas />
         <Footer />
+        <Cart />
       </BrowserRouter>
     </Provider>
   )
